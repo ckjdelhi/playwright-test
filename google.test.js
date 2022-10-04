0d09b8ca741db0d4196c3e541802a9c4e268279d
ckjdelhi <chandan.krk@gmail.com>
2022-10-04 19:16:46 +0530

    added

--- google.test.js
+++ google.test.js
@@ -1,18 +1,20 @@
 const fs = require("fs");
 jest.setTimeout(40 * 1000)
 
-const dataInput = [
-    "HCL Tech",
-    "IBM"
-];
+// const dataInput = [
+//     "HCL Tech",
+//     "IBM"
+// ];
+const dataInput = fs.readFileSync("data.csv", "utf8").split("\r\n");
 describe("Extract Data", () => {
     dataInput.forEach(company =>{
     it("Get Company URL", async () => {
         await page.goto("https://www.google.com/");
         await page.type('input[name="q"]', company);
         await page.click(`[name='btnI']`);
-        await page.waitForURL(await page.url());
-        const d = company+","+(await page.url());
+       // await page.waitForURL(await page.url());
+        const d = company+"|"+(await page.url());
+        console.log(d)
         writeLine(d)
       
       });   
